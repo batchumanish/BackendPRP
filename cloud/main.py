72c0b9c0c9a8b460af756e372fbbd975566f1528
B-Manish <batchumanish@gmail.com>
2024-03-16 23:52:11 +0530

    connected to cloud db

--- cloud/main.py
+++ cloud/main.py
@@ -1,3 +1,4 @@
+from fastapi import FastAPI
 from pymongo import MongoClient
 
 client=MongoClient('mongodb+srv://manishbatchu:Q7h5KtERcGZdfJ4E@manishcluster.71tedsw.mongodb.net/')
@@ -7,3 +8,16 @@ db=client.get_database('manishclouddb')
 records=db.restdata
 
 print(records.count_documents({}))
+# Initialize FastAPI app
+user = FastAPI()
+
+
+
+@user.get("/data")
+async def get_data():
+    data =records.count_documents({})
+
+    if data:
+        return {"status": "success", "data": data}
+    else:
+        return {"status": "error", "message": "Data not found"}
